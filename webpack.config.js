e34f211058c21c462e05ccba073113b8e35f962a
Ilyas Salhi <ilyas.salhi@gmail.com>
2025-01-26 22:25:20 -0700

    got rid of the archive

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,63 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: 'http://localhost:8080/dist/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              url: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'img/[name][ext]',
-          publicPath: 'http://localhost:8080/dist/'
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    hot: true,
-    port: 8080,
-    static: {
-      directory: path.join(__dirname, 'dist'),
-      publicPath: '/dist'
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  const publicPath = isDevelopment ? 'http://localhost:8080/dist/' : './dist/';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: publicPath
     },
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  target: 'web'
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                url: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'img/[name][ext]',
+            publicPath: publicPath
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    devServer: {
+      hot: true,
+      port: 8080,
+      static: {
+        directory: path.join(__dirname, 'dist'),
+        publicPath: '/dist'
+      },
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    target: 'web'
+  };
 };
